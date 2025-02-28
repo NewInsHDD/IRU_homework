60600327640d273c417bab814917c25c910465fd
szasomaa@gmail.com <szasomaa@gmail.com>
2025-02-28 16:42:32 +0100

    Added commit 1 after merge

--- IRU_homework/main.cpp
+++ IRU_homework/main.cpp
@@ -6,5 +6,6 @@ int main(int argc, char* argv[]) {
 	std::cout << "NEW COMMIT 2" << std::endl;
 	std::cout << "NEW COMMIT 3" << std::endl;
 	std::cout << "NEW COMMIT 4" << std::endl;
+	std::cout << "AFTER MERGE COMMIT 1" << std::endl;
 	return 0;
 }
