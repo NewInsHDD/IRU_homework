858f2baa5f5b8c55a0cb23f8bf0597e89591c4ba
szasomaa@gmail.com <szasomaa@gmail.com>
2025-02-28 15:32:37 +0100

    Added new commit 3

--- IRU_homework/main.cpp
+++ IRU_homework/main.cpp
@@ -4,5 +4,6 @@ int main(int argc, char* argv[]) {
 	std::cout << "HELLO" << std::endl;
 	std::cout << "NEW COMMIT 1" << std::endl;
 	std::cout << "NEW COMMIT 2" << std::endl;
+	std::cout << "NEW COMMIT 3" << std::endl;
 	return 0;
 }
